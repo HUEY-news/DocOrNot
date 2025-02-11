6387a9dcc6339315a97c2282d061598f75d2f49e
Саид Исмаилов <mazgOFF.net@mail.ru>
2025-02-11 13:55:06 +0300

    presentation: Сверстал экран подключения к порталу.

--- app/src/main/java/com/houston/docornot/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/houston/docornot/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.houston.docornot.ui.theme
+package com.houston.docornot.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/houston/docornot/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/houston/docornot/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.houston.docornot.ui.theme
+package com.houston.docornot.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
